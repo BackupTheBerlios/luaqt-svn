f3433d04cdf52e9c750b0c59018b469250f12f75
punto <punto@4c9b881e-cd06-0410-8f74-907230868df1>
2006-02-13 19:27:07 +0000

    changed build scripts to comply with new naming scheme

--- external_tools/lua_qt4/config_linux.py
+++ external_tools/lua_qt4/config_linux.py
@@ -1,5 +1,9 @@
-## The lua libraries. On debian, they are called '...lua50'
-#lua_libs = ['lua51']
+## The lua libraries. On debian, they are called '...lua5.1'
+lua_libs = ['lua5.1'] # we don't link with lua, the symbols should be exported by the host program
+
+## The tolua++ libraries, default is ['tolua++5.1']
+#tolua_libs = ['tolua++5.1']
+
 
 ## On debian, the lua headers are on lua50/
-extra_cxx_flags = ['-I/usr/local/include/lua51']
+extra_cxx_flags = ['-I/usr/include/lua5.1']
